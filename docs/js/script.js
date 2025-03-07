337eb395e11696fee4f84439f40e1a1c177db6f5
matcha186 <136787058+matcha186@users.noreply.github.com>
2025-03-07 16:45:20 +0900

    Add files via upload

---
+++ docs/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".slider").slick({
+      autoplay: true,
+      dots: true,
+      prevArrow: '<button class="arrow-common arrow-prev"></button>',
+      nextArrow: '<button class="arrow-common arrow-next"></button>',
+    });
+  });
