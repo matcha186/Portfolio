a5182384797289f82d4d51b0324fe60b64f1ab43
matcha186 <136787058+matcha186@users.noreply.github.com>
2025-03-10 16:04:21 +0900

    Add Scroll

--- docs/js/script.js
+++ docs/js/script.js
@@ -33,3 +33,15 @@ $(document).ready(function(){
       $('#game-slide-caption').html(game_captions[currentSlide]);
   });
 });
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var header = $('nav').height();
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - header;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
