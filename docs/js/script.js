43ff49f43bef4fe78e8fa87c789847e1aea4933e
matcha186 <136787058+matcha186@users.noreply.github.com>
2025-03-13 17:00:43 +0900

    Stop audio when another audio starts playing

--- docs/js/script.js
+++ docs/js/script.js
@@ -47,3 +47,18 @@ $(function(){
     return false;
   });
 });
+
+// 他のaudioが流れたら今流れているものを止める
+document.addEventListener("DOMContentLoaded", function () {
+  const audioElements = document.querySelectorAll("audio");
+
+  audioElements.forEach(audio => {
+      audio.addEventListener("play", function () {
+          audioElements.forEach(otherAudio => {
+              if (otherAudio !== audio) {
+                  otherAudio.pause();
+              }
+          });
+      });
+  });
+});
